b278988a09434098fe6b19777e6e85f52e8647c7
USER <USER@AMP-03>
2019-04-18 17:40:56 +0900

    commit

--- Chap09-fig9-03/src/SwingEx.java
+++ Chap09-fig9-03/src/SwingEx.java
@@ -7,12 +7,12 @@ public class SwingEx extends JFrame {
 		setTitle("스윙 컴포넌트 보이기 예제");
 		createMenu();
 		createToolBar();
-		createSplitpane();
+		createSplitPane();
 		setSize(500,400);
 		setVisible(true);
 		
 	}
-	private void createMenu();{
+	private void createMenu() {
 		JMenuBar mb = new JMenuBar();
 		mb.add(new JMenu("File"));
 		mb.add(new JMenu("Edit"));
@@ -26,11 +26,31 @@ public class SwingEx extends JFrame {
 		JToolBar bar = new JToolBar("kitae Menu");
 		
 		bar.setBackground(Color.LIGHT_GRAY);
-		bar.add(new)
+		bar.add(new JButton("New"));
+		bar.add(new JButton(new ImageIcon("images/open,jpg")));
+		bar.addSeparator();
+		bar.add(new JButton(new ImageIcon("images/save.jpg")));
+		bar.add(new JLabel("search"));
+		bar.add(new JTextField("text field"));
+		
+		JComboBox<String> combo = new JComboBox<String>();
+		combo.addItem("Java");
+		combo.addItem("c#");
+		combo.addItem("c");
+		combo.addItem("c++");
+		bar.add(combo);
+		add(bar, BorderLayout.NORTH);
+	}
+	
+	private void createSplitPane(){
+		String [] fruits= {"apple", "banana", "kiwi", "mango", "pear", "peach", "berry", "strawberry", "blackberry"};
+		JList<String> scrollList = new JList<String> (fruits);
+		JSplitPane pane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, new JScrollPane(scrollList), new JScrollPane(new JLabel( new ImageIcon ("images/img1.jpg"))));
+		add(pane, BorderLayout.CENTER );
 	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+			new SwingEx();
 
 	}
 
