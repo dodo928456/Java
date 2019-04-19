d5168a4d71dfde2fc2a2d86fdf5e78673e020ee6
승열 <@LG>
2019-04-20 00:07:22 +0900

    commit

---
+++ Chap09-fig9-13/src/GridLayoutSample.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+public class GridLayoutSample extends JFrame {
+	
+	public GridLayoutSample(){
+		setTitle("GridLayout Sample");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		GridLayout grid = new GridLayout(4,3,5,5);
+		
+		Container c = getContentPane();
+		c.setLayout(grid);
+		for(int i=1; i<=9; i++)
+			c.add(new JButton(Integer.toString(i)));
+		
+		
+		c.add(new JButton("*"));
+		c.add(new JButton("0"));
+		c.add(new JButton("#"));
+		
+		setSize(500, 500);
+		setVisible(true);
+		
+	}
+
+	public static void main(String[] args) {
+		new GridLayoutSample();
+
+	}
+
+}
