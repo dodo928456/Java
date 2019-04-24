12b4296195cb8352ae84e7f74936e91c7d5815f9
USER <USER@AMP-03>
2019-04-24 18:20:30 +0900

    Commit

---
+++ chap11-ex09/src/TextAreaEx.java
@@ -0,0 +1,36 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+public class TextAreaEx extends JFrame {
+	private JTextField tf = new JTextField(20);
+	private JTextArea ta = new JTextArea(7, 20);
+		
+	public TextAreaEx(){
+		setTitle("텍스트 만들기 예제");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		c.setLayout(new FlowLayout());
+		
+		c.add(new JLabel("입력수 <enter>키를 입력하세요"));
+		c.add(tf);
+		c.add(new JScrollPane(ta));
+		
+		tf.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				JTextField t = (JTextField)e.getSource();
+				ta.append(t.getText() + "\n");
+				t.setText("");
+				
+			}
+			
+		});
+		setSize(300,300);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new TextAreaEx();
+	}
+
+}
